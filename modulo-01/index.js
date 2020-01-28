452121185469904089828a9b9a17d3b3761c3c74
maycon8609 <maycon8609@gmail.com>
2020-01-28 00:02:54 -0300

    modulo 1 conceitos node api rest

---
+++ modulo-01/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+// Query params = ?teste=1
+// Route params = /users/1
+// Request body = {"name": "maycon", "key": "1234"}
+
+// CRUD = Create, Read, Update, Delete
+
+const users = ['maycon', 'antonio', 'robson'];
+
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
+
+server.get('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  return res.json(users[index]);
+});
+
+server.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
+});
+
+server.listen(3000);
