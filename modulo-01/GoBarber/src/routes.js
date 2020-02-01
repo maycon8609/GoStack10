1cc4785372fe110391c20174023d0e9c9df4153f
maycon8609 <maycon8609@gmail.com>
2020-02-01 15:22:40 -0300

    erro

--- modulo-01/GoBarber/src/routes.js
+++ modulo-01/GoBarber/src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/teste', (req, res) => {
-  return res.json({ mesage: 'Hello RocketSeat ' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'mayconsilva',
+    email: 'maycon@gmail.com',
+    password_hash: '12345678',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
