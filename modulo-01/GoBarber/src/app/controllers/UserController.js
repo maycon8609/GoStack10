7c40fbbc5373cc1b9a18d98de850cd949140164f
maycon8609 <maycon8609@gmail.com>
2020-02-01 17:24:55 -0300

    cadastro de usuários

--- modulo-01/GoBarber/src/app/controllers/UserController.js
+++ modulo-01/GoBarber/src/app/controllers/UserController.js
@@ -4,8 +4,9 @@ class UserController {
   async store(req, res) {
     const userExists = await User.findOne({ where: { email: req.body.email } });
 
-    if (userExists)
+    if (userExists) {
       return res.status(400).json({ error: 'User already exists.' });
+    }
 
     const { id, name, email, provider } = await User.create(req.body);
 
