a5d46c75c0a4b995757a1f9ae6b1154bfde476a3
maycon8609 <maycon8609@gmail.com>
2020-02-03 21:20:35 -0300

    autenticação de usuario

--- week_1/desafio-2/backend/src/database/seeds/20200202225134-create-seeders.js
+++ week_1/desafio-2/backend/src/database/seeds/20200202225134-create-seeders.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 module.exports = {
   up: QueryInterface => {
     return QueryInterface.bulkInsert(
-      'users',
+      'requests',
       [
         {
           name: 'Distribuidora FastFeet',
@@ -18,6 +18,6 @@ module.exports = {
   },
 
   down: queryInterface => {
-    return queryInterface.dropTable('users');
+    return queryInterface.dropTable('requests');
   },
 };
