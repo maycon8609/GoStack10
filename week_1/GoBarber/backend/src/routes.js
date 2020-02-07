4c13888ad65b16d247e1dd6911eabb4387b1cbf0
maycon8609 <maycon8609@gmail.com>
2020-02-07 06:26:04 -0300

    relationship between tables | avatar_id

--- week_1/GoBarber/backend/src/routes.js
+++ week_1/GoBarber/backend/src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleWare from './app/middlewares/auth';
 
@@ -18,7 +19,5 @@ routes.use(authMiddleWare);
 
 routes.put('/users', UserController.update);
 
-routes.post('/files', upload.single('file'), (req, res) => {
-  return res.json({ ok: true });
-});
+routes.post('/files', upload.single('file'), FileController.strore);
 export default routes;
