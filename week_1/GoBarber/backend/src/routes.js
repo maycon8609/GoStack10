7a55f231332ee15d65d59b8524cca002864ca087
maycon8609 <maycon8609@gmail.com>
2020-02-07 07:02:44 -0300

    index providers

--- week_1/GoBarber/backend/src/routes.js
+++ week_1/GoBarber/backend/src/routes.js
@@ -6,6 +6,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
 
 import authMiddleWare from './app/middlewares/auth';
 
@@ -18,6 +19,8 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleWare);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.strore);
 export default routes;
