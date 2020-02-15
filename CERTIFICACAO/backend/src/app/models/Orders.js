bf1677e10ca6a72544159def2c4bc20f40f09aaf
maycon8609 <maycon8609@gmail.com>
2020-02-15 16:28:04 -0300

    Orders migration & model

---
+++ CERTIFICACAO/backend/src/app/models/Orders.js
@@ -0,0 +1,36 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Orders extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        recipient_id: Sequelize.INTEGER,
+        deliveryman_id: Sequelize.INTEGER,
+        signature_id: Sequelize.INTEGER,
+        product: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    return this;
+  }
+
+  static associate(models) {
+    this.belongsTo(models.Recipients, {
+      foreignKey: 'recipient_id',
+      as: 'recipient',
+    });
+    this.belongsTo(models.Deliveryman, {
+      foreignKey: 'deliveryman_id',
+      as: 'deliveryman',
+    });
+    this.belongsTo(models.Signature, {
+      foreignKey: 'signature_id',
+      as: 'signature',
+    });
+  }
+}
+
+export default Orders;
---
+++ CERTIFICACAO/backend/src/database/migrations/20200215182039-create-orders.js
@@ -0,0 +1,62 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('orders', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      recipient_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'recipients', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      deliveryman_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'deliverymans', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      signature_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: { model: 'files', key: 'id' },
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+      },
+      product: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      canceled_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defalt: null,
+      },
+      start_date: {
+        type: Sequelize.DATE,
+        defalt: null,
+      },
+      end_date: {
+        type: Sequelize.DATE,
+        defalt: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('orders');
+  },
+};
